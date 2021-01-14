b1179915f666561d47475575a71e4d06599628f3
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-13 19:51:55 -0800

    commit tree 9499c52664286b66f2c4a49234146df6643dad8d

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name=(dog_name) # setter method
+        @name = dog_name
+    end
+
+    def name # getter method
+        @name
+    end
+
+    def breed=(dog_breed) # setter method
+        @breed = dog_breed
+    end
+
+    def breed # getter method
+        @breed
+    end
+
+end
