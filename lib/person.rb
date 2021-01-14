13f16eace52512576f2d08ab24ebc3737e827bd5
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-13 19:52:04 -0800

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def name=(person_name) # setter method
+        @name = person_name
+    end
+
+    def name # getter method
+        @name
+    end
+
+    def job=(person_job) # setter method
+        @job = person_job
+    end
+
+    def job # getter method
+        @job
+    end
+
+end
